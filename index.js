52f9f86a262fa1814d5cb237edf575bd90933de9
Luca Béla Palkovics <KoKuToru@users.noreply.github.com>
2016-08-26 10:35:39 +0200

    support preprocess

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
     this.isProductionBuild = app.env === 'production';
-    this.options = generateOptions(app.options.transforms || {});
+    this.preOptions = generateOptions(app.options.transforms || {}, true);
+    this.postOptions = generateOptions(app.options.transforms || {}, false);
   },
 
   postprocessTree(type, tree) {
-    return new TransformFilter(tree, this.options);
+    return new TransformFilter(tree, this.postOptions);
+  },
+  preprocessTree(type, tree) {
+    return new TransformFilter(tree, this.preOptions);
   }
 };
 
-function generateOptions(options) {
+function generateOptions(options, preprocess) {
   if (options.extensions && !Array.isArray(options.extensions)) {
     throw new Error('`transforms.extensions` must be an array of file extensions');
   }
@@ -26,8 +30,12 @@ function generateOptions(options) {
   if (options.targets && options.targets.find(t => !t.pattern)) {
     throw new Error('`transforms.targets`: all transforms require a `pattern` string');
   }
-  return Object.assign({
+  var tmp = Object.assign({
     targets: [],
     extensions: ['html', 'js', 'css']
   }, options);
+  tmp.targets = tmp.targets.filter(function (x) {
+    return preprocess ? x.preprocess : !x.preprocess;
+  });
+  return tmp;
 }
